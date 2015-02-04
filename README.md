5f6946a2347656df31a49179000470a9556ce8fa
Xing Xing <mikespook@gmail.com>
2015-02-04 16:24:01 +0800

    written introductions

--- README.md
+++ README.md
@@ -1,8 +1,77 @@
 # STS
-STS: Secure Tunnel Server,  a configurable and powerful ssh tunnel server.
+
+STS: Secure Tunnel Server - a configurable and powerful ssh tunnel server.
+
+# Motivation
+
+# Installation
+
+	$ go get github.com/mikespook/sts
+
+# Configuration
+
+Configuration file should be in YAML format.
+
+Here is an example:
+
+```yaml
+addr: 127.0.0.1:2222
+auth:
+	anonymous: false
+	password: static://123456
+	pubkey: file://id_rsa.pub
+keys: [ id_rsa ]
+log:
+	file:
+	level: all
+pwd: ../misc
+```
+
+	* addr: "stsd" will bind to this address (and port)
+	* auth: See below
+	* keys: Private keys used by server
+	* log: See below
+	* pwd: Current working directory
+
+## Authorization
+
+stsd has three auth options: anonymous, password and pubkey.
+
+When `anonymous` is set to `I Understand the Risks`(DANGER!!!), other options will be ignored and there will be no authenticated mechanism to stsd. `password` and `pubkey` should be one of following formats:
+
+	* `static://[string]`, the string will be used as password/pubkey directly;
+	* `file://[file]`, the file will be read and its contents will be used as password/pubkey;
+	* `rpc://[url]`, connect to a rpc server witch can serve two remote calls: 1) STS.PasswordAuth 2) STS.PublicKeyAuth. And "url" should be in form `(tcp|http)://[ip]:[port]/[path]`.
+
+## Log
+
+In the default way, the output of log will print to stdout. In case of `file` is a legal path, Log will output records to the file. Levels can be combined with `|` from: `error`, `warning`, `message` and `debug`. Also, there are two magic values `all` and `none` to display all levels or nothing.
+
+# Usage
+
+Running stsd:
+
+	$ stsd -config=../misc/config.yaml
+
+Establish local ssh tunnel proxy:
+
+	$ ssh -p 2222 127.0.0.1 -D18081
+
+Using SOCKS5 proxy:
+
+	$ curl --socks5 127.0.0.1:18081 http://www.mikespook.com
 
 # TODO
 
- * Client status: connections, R/W bytes, start/established time
- * Authorization mechanism
+ * Session status: connections, R/W bytes, start/established time
  * Server status: active clients, R/W bytes, start/established time
+
+# Contributors
+
+(_Alphabetic order_)
+ 
+ * [Xing Xing][blog] <mikespook@gmail.com> [@Twitter][twitter]
+
+# Open Source - MIT Software License
+
+See LICENSE.
--- stsd/intro.go
+++ stsd/intro.go
@@ -1,33 +1,73 @@
 package main
 
-var intro = `
-STS: Secure Tunnel Server
+const intro = `
+STS: Secure Tunnel Server - MIT Software License 
 
-Author: Xing Xing <mikespook@gmail.com>
+	Xing Xing <mikespook@gmail.com>
+	http://github.com/mikespook/sts
 
-Configuration file is using YAML format.
+CONFIGURATION
 
-Here is an example:
+	Config file should be in YAML format.
+
+	Here is an example:
+
+---------Example Start----------
 
 addr: 127.0.0.1:2222
-keys:
-	id_rsa_1
-	id_rsa_2
-	id_rsa_3
+auth:
+	anonymous: false
+	password: static://123456
+	pubkey: file://id_rsa.pub
+keys: [ id_rsa ]
 log:
-	file: /var/log/stsd.log
+	file:
 	level: all
+pwd: ../misc
 
-And the meaning of fields are:
+----------Example End-----------
 
-	* addr - What address and port stsd will bind to
-	* keys - Private keys used by server
-	* log  - Log configuration
-	* log-file  - Log file, leave this empty to output to stdout
-	* log-level - Log level: 'error', 'warning', 'message', 'debug', 'all' and
-		'none',	levels can be combined with '|'
+	* addr: "stsd" will bind to this address (and port)
+	* auth: See "AUTHORIZATION" for more details
+	* keys: Private keys used by server
+	* log: Log configuration
+	* pwd: Current working directory
 
-Running stsd:
+AUTHORIZATION
 
-$ stsd -config=config.yaml
+	stsd has three auth-options: anonymous, password and pubkey.
+
+	When "anonymous" is set to "I Understand the Risks"(DANGER!!!), other 
+	options will be ignored and there will be no authenticated mechanism to stsd.
+
+	"password" and "pubkey" should be one of following formats:
+
+	* "static://[string]", the string will be used as password/pubkey directly;
+	* "file://[file]", the file will be read and its contents will be used as 
+		password/pubkey;
+	* "rpc://[url]", connect to a rpc server witch can serve two remote calls:
+		1) STS.PasswordAuth 2) STS.PublicKeyAuth. And "url" should be in form 
+		"(tcp|http)://[ip]:[port]/[path]".
+
+LOG
+
+	In the default way, the output of log will print to stdout. In case of
+	"file" is a legal path, Log will output records to it. Levels can be 
+	combined with "|" from following values: "error", "warning", "message",
+	"debug". Also, there are two magic values "all" and "none" to display all
+	levels or nothing.
+
+USAGE
+
+	Running stsd:
+
+	$ stsd -config=config.yaml
+
+	Establish local ssh tunnel proxy:
+
+	$ ssh -p 2222 127.0.0.1 -D18081
+
+	Using SOCKS5 proxy:
+
+	$ curl --socks5 127.0.0.1:18081 http://www.mikespook.com
 `
